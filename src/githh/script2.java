aebe29af2b9899711da39cf757fd7e21be38040e
Dell <Dell@DESKTOP-MQA8AAP.mshome.net>
2024-08-15 02:27:10 +0530

    commit 2

---
+++ src/githh/script2.java
@@ -0,0 +1,25 @@
+package githh;
+
+import org.testng.annotations.Test;
+
+public class script2 
+{
+
+	 @Test
+			public void rev()
+			{
+			String s1="reva";
+			String rev="  ";
+			for(int i=s1.length()-1;i>=0;i--)
+			{
+				rev=rev+s1.charAt(i);
+			}
+			
+			
+
+		}
+		
+
+	}
+
+
